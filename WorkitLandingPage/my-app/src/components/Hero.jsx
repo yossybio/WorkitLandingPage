526719ae6def5411c8e096c6e96784479eef79ae
yossybio <yossybio@gmail.com>
2025-02-16 19:49:08 +0200

    Complete mobile version

--- WorkitLandingPage/my-app/src/components/Hero.jsx
+++ WorkitLandingPage/my-app/src/components/Hero.jsx
@@ -31,27 +31,14 @@ export default function Hero() {
       <div className={StyleSheet.mainContent}>
         <div className={StyleSheet.content}>
           <h1 className={StyleSheet.caption}>
-            Data <span>tailored</span> <br className={StyleSheet.newLine}/>to <br className={StyleSheet.newLine}/>your needs.
+            Data <span>tailored</span> <br className={StyleSheet.newLine} />
+            to <br className={StyleSheet.newLine} />
+            your needs.
           </h1>
           <ButtonPrimary name="Learn more" />
         </div>
       </div>
       <Ornamental />
-      {/* <svg
-        className={StyleSheet.ornamental}
-        width="100vw"
-        height="7.2rem"
-        viewBox="0 0 1440 72"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <path
-          id="ornamental"
-          d="M0 0L0 72L1440 72L1440 0C1440 0 1082.96 68 722 68C361.039 68 0 0 0 0L0 0Z"
-          fill="#FCF8FF"
-          fillRule="evenodd"
-        />
-      </svg> */}
     </main>
   );
 }
